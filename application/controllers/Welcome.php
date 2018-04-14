efb8b57d36ea48709e76cf8c455f78e7968537b0
suraj_shinde\SURAJ <suraj@webosys.com>
2018-04-14 17:35:53 +0530

    demo purpose

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
 class Welcome extends CI_Controller {
 
 	/**
@@ -22,6 +21,5 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
-                $this->load->view('hello');
 	}
 }
