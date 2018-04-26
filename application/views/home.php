b3e85fce0c81215c1929eaa0150b7dcaa440c68b
suraj_shinde\SURAJ <suraj@webosys.com>
2018-04-26 23:55:37 +0530

    homepage test

--- application/views/home.php
+++ application/views/home.php
@@ -165,25 +165,25 @@
              padding:15px;
          }
      </style>
-    <div class="banner-container"> 
+<!--    <div class="banner-container"> 
   	<div id="carousel" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carousel" data-slide-to="0" class="active"></li>
     <li data-target="#carousel" data-slide-to="1" ></li>
     <li data-target="#carousel" data-slide-to="2"></li>
   </ol>
-  <!-- Carousel items -->
+   Carousel items 
   <div class="carousel-inner">
     <div class="item active"><img src="<?php echo base_url(); ?>assets/images/a.jpg"  width="100%" height="50%" alt="banner" /></div>
     <div class="item"><img src="<?php echo base_url(); ?>assets/images/b.jpg"  width="100%" height="50%" /></div>
     <div class="item"><img src="<?php echo base_url(); ?>assets/images/c.jpg" width="100%" height="50%" alt="banner" /></div>
   </div>
   
-  <!-- Carousel nav -->
+   Carousel nav 
   <a class="carousel-control left" href="#carousel" data-slide="prev">&lsaquo;</a>
   <a class="carousel-control right" href="#carousel" data-slide="next">&rsaquo;</a>
 </div>	
-  </div>
+  </div>-->
         
         <div class="row" style="background:#F2F3F4">
         <div class="container">        
