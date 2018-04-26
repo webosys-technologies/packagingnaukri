22ae813d006d36ae231305ae3e9996e8f54af613
suraj_shinde\SURAJ <suraj@webosys.com>
2018-04-27 00:18:05 +0530

    homepage design issue

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -16,9 +16,9 @@ class Home extends CI_Controller
 	function index()
 	{
 	    	
-            $this->load->view('home_header');
+//            $this->load->view('home_header');
             $this->load->view('home');
-            $this->load->view('home_footer');
+//            $this->load->view('home_footer');
             
 	}
         function services()
