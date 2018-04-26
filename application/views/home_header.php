cb05bb3b947c142556fa8c81d570b93bc3a7e3ba
suraj_shinde\SURAJ <suraj@webosys.com>
2018-04-27 00:10:10 +0530

    adding bootstrap lib for homepage issue

--- application/views/home_header.php
+++ application/views/home_header.php
@@ -5,6 +5,14 @@
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">    
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
   <script src="<?php echo base_url()?>assets/jquery/jQuery-2.1.4.min.js"></script>
   <script src="<?php echo base_url()?>assets/jquery/jquery.flexslider.js"></script>
