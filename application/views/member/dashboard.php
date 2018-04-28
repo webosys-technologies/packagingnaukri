a8b0fe8b3103e54a01f33b660144ac7329b33d03
suraj_shinde\SURAJ <suraj@webosys.com>
2018-04-29 00:55:26 +0530

    member dashboard

---
+++ application/views/member/dashboard.php
@@ -0,0 +1,20 @@
+
+<!--next here sunil-->
+<!DOCTYPE html>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+          <li><a href="<?php echo base_url();?>student/Dashboard"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
+
+
+  </div>
+ 
---
+++ application/views/member/footer.php
@@ -0,0 +1,18 @@
+
+
+    <footer class="main-footer">
+<!--        <div class="pull-right hidden-xs">
+          <b>Webosys Technologies</b> Student System
+        </div>-->
+        <strong>Copyright &copy; 2018 <a href="http://webosys.com/">Webosys Technologies</a>.</strong> All rights reserved.
+    </footer>
+    
+     <!--Bootstrap 3.3.7--> 
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+     <!--<link rel="stylesheet"href="<?php echo base_url();?>assets/fonts/css/font-awesome.min.css">-->
+     <script src="<?php echo base_url(); ?>assets/js/bootstrap-auto-dismiss-alert.js"></script>
+ <script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+ <!--AdminLTE App-->
+<script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>
+  </body>
+</html>
