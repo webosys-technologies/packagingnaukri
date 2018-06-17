e3e725283d3dfd5e94df6a2fe25a9b8a800f47f1
Sunil Khandare <sunil@webosys.com>
2018-06-17 10:39:24 +0530

    Home issue

--- application/views/home.php
+++ application/views/home.php
@@ -29,7 +29,7 @@
         
         <div class="row" id="content_body" style="background:#F2F3F4">
         <div class="container">        
-		<div class="col-md-9 col-xs-9 col-md-offset-1">
+		<div class="col-md-9 col-xs-9">
 		<h3 style="color:#02B645;">Enjoy building your career with lots of ready jobs!</h3>
 			<p>To provide the right opportunity to the every qualified packaging professional.</p>   
 		</div>  <br>
@@ -41,7 +41,7 @@
             </div>
          <div class="row" id="content_body" style="background:white;">        
 		<div class="container">
-                    <div class="col-md-9 col-md-offset-1">
+                    <div class="col-md-9">
 		<h4 style="color:#02B645;">Welcome to Packaging Naukri</h4><hr style="border-top: 1px solid #ccc;">
 			
 <h5 style="color:#02B645;">Objective</h5>
@@ -50,7 +50,7 @@ We intend and aim to place the candidate as per the precise need of the position
 
 Packaging and related businesses has seen a phenomenal growth trajectory in the last decade and the momentum is increasing on a daily basis and so is the need for qualified and experienced professionals on this front. Being a vast and diversified field it is difficult to find talent suited to a particular requirement with the required expertise. This is were we step in.<br><br>
 
-To provide the right opportunity to the every qualified packaging professional, to achieve his/her career objective. At the same time to provide the suitable candidate as per employer’s business need and develop a win-win relationship</p>   
+To provide the right opportunity to the every qualified packaging professional, to achieve his/her career objective. At the same time to provide the suitable candidate as per employerâ€™s business need and develop a win-win relationship</p>   
                     </div>
                     </div> <br><br>
            
