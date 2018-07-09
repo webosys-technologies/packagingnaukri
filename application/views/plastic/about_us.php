112c01dbccbd0399653ce4e776687cb5d4dde9c8
kadampawan <pawan@webosys.com>
2018-07-09 23:45:01 +0530

    home page changes for printing and plastic

--- application/views/plastic/about_us.php
+++ application/views/plastic/about_us.php
@@ -17,18 +17,25 @@
 		<div class="col-md-10 col-md-offset-1">
 		<h5 style="color:#0BA1DC;">Home <i class="fas fa-angle-double-right"></i> About Us</h5>
                 <p>
-                    We are a team of Plastic professionals, by qualification and by experience, with varied expertise in Plastic materials , processes, Consultancy, New product development, molds and machineries. We have a deep reach in to the Plastic industry due to associations with various Plastic related organizations and institutes. This has helped us create a vast array of database of talents which can fulfill specific requirement of the industry. We also have HR professionals as a part of our team to analyze aspects in a candidate other than technical.
+                    We are a team of Plastic professionals by qualification and by experience, with varied expertise in Plastic materials , processes, Consultancy, New product development, molds and machineries. We have deep reach in to the Plastic industry because of our associations with various Plastic related organizations and institutes. This has helped us create a vast array of talent database which can fulfill specific requirement of the industry. We also have HR professionals as a part of our team to analyze aspects in a candidate other than technical.
 <br><br>
 As Plastic professionals we can understand requirements of the client in absolute sense and thus can provide talent options precisely suited to it and this is were we can make a difference.
                 </p>
                 <hr>
 			
 <h5 style="color:#0BA1DC;">Key Strengths</h5>
-<li>Team of qualified & experienced Plastic professionals, HR professionals directly involved in recruitment, screening and interviewing process.</li>
+<li>Flexible Solution Design</li>
+<li>Experience Recruitment Engine</li>
+<li>High quality hire from a wide talent pool</li>
+<li>Compliance & Diversity</li>
+<li>Technology Infrastructure</li>
+<li>Geographical footprint that extends across India,Middle east</li>
+<li>Enhanced reporting</li>
+<!-- <li>Team of qualified & experienced Plastic professionals, HR professionals directly involved in recruitment, screening and interviewing process.</li>
 <li>Exclusive Plastic professionals recruitment firm.</li>
-<li>Largest database and networking of Plastic professionals.</li>
+<li>Largest database and network of Plastic professionals.</li>
 <li>Members of various Plastic communities.</li>
-<li>Readily available talent pool.</li>
+<li>Readily available talent pool.</li> -->
 		</div>  <br>
            
 		
